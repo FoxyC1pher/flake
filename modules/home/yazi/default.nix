e2b6bcd01c89afa467c3d880c53450159ca0126a
FoxyChipher <ageev-eldar@mail.ru>
2026-03-30 17:31:13 +0300

    Yazi Fixed, Finally

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -1,19 +1,30 @@
-{ lib, config, pkgs, inputs, vars, ... }:
 {
-  imports = [
-    ./initLua.nix
-    ./keymap.nix
-    ./plugins.nix
-    ./settings.nix
-  ];
-  
+	# lib,
+	# config,
+	# pkgs,
+	inputs,
+	vars,
+	...
+}: {
+	imports = [
+		./initLua.nix
+		./keymap.nix
+		./plugins.nix
+		./settings.nix
+	];
+
 	home-manager = {
-		extraSpecialArgs = { inherit inputs vars; };
-		users.${vars.userName} =  { config, pkgs, lib, ... }: {
+		extraSpecialArgs = {inherit inputs vars;};
+		users.${vars.userName} = {
+			# config,
+			# pkgs,
+			# lib,
+			...
+		}: {
 			programs.yazi = {
 				enableFishIntegration = true;
-				enableBashIntegration = true;	
-				enable = true;	
+				enableBashIntegration = true;
+				enable = true;
 			};
 		};
 	};
--- modules/home/yazi/settings.nix
+++ modules/home/yazi/settings.nix
@@ -1,30 +1,54 @@
-{ lib, config, pkgs, inputs, vars, ... }:
 {
+	# lib,
+	# config,
+	# pkgs,
+	inputs,
+	vars,
+	...
+}: {
 	home-manager = {
-		extraSpecialArgs = { inherit inputs vars; };
-		users.${vars.userName} =  { config, pkgs, lib, ... }: {
+		extraSpecialArgs = {inherit inputs vars;};
+		users.${vars.userName} = {
+			config,
+			# pkgs,
+			# lib,
+			...
+		}: {
 			programs.yazi = {
 				settings = {
-									
-					manager = {
+					mgr = {
 						show_hidden = true;
 						sort_by = "natural";
 						sort_dir_first = true;
+						sort_reverse = false;
 					};
-					
+
 					preview = {
 						max_width = 1200;
 						max_height = 900;
 						cache_dir = "${config.xdg.cacheHome}/yazi";
 					};
-					
+
 					plugin = {
 						prepend_fetchers = [
-							{ id = "mime"; url = "local://*"; run = "mime-ext.local"; prio = "high"; }
-							{ id = "mime"; url = "remote://*"; run = "mime-ext.remote"; prio = "high"; }
+							{
+								id = "mime";
+								url = "local://*";
+								run = "mime-ext.local";
+								prio = "high";
+							}
+							{
+								id = "mime";
+								url = "remote://*";
+								run = "mime-ext.remote";
+								prio = "high";
+							}
 						];
 						prepend_previewers = [
-							{ name = "*.md"; run = "glow"; }
+							{
+								name = "*.md";
+								run = "glow";
+							}
 						];
 					};
 				};
