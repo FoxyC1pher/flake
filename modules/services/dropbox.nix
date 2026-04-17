5761abb0522b1f79f7678e332dd2ff7db36f3e47
FoxyChipher <ageev-eldar@mail.ru>
2026-04-17 05:17:22 +0300

    dropbox

--- modules/services/dropbox.nix
+++ modules/services/dropbox.nix
@@ -5,28 +5,29 @@
 }: {
 	home-manager = {
 		extraSpecialArgs = {inherit inputs vars;};
-		users.${vars.userName} = {pkgs, ...}: {
+		users.${vars.userName} = {...}: {
+			services.dropbox.enable = true;
 			# Сервис Dropbox через Home Manager
-			systemd.user.services.dropbox = {
-				Unit = {
-					Description = "Dropbox service";
-					After = ["network.target" "graphical-session.target"];
-				};
-				Install = {
-					WantedBy = ["default.target"];
-				};
-				Service = {
-					ExecStart = "${pkgs.dropbox}/bin/dropbox";
-					Restart = "on-failure";
-					RestartSec = 10;
-					# Опционально: окружение для графики и Qt
-					Environment = [
-						#"DISPLAY=:0"
-						"QT_PLUGIN_PATH=${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}"
-						"QML2_IMPORT_PATH=${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtQmlPrefix}"
-					];
-				};
-			};
+			# systemd.user.services.dropbox = {
+			# 	Unit = {
+			# 		Description = "Dropbox service";
+			# 		After = ["network.target" "graphical-session.target"];
+			# 	};
+			# 	Install = {
+			# 		WantedBy = ["default.target"];
+			# 	};
+			# 	Service = {
+			# 		ExecStart = "${pkgs.dropbox}/bin/dropbox";
+			# 		Restart = "on-failure";
+			# 		RestartSec = 10;
+			# 		# Опционально: окружение для графики и Qt
+			# 		Environment = [
+			# 			#"DISPLAY=:0"
+			# 			"QT_PLUGIN_PATH=${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+			# 			"QML2_IMPORT_PATH=${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtQmlPrefix}"
+			# 		];
+			# 	};
+			# };
 		};
 	};
 }
