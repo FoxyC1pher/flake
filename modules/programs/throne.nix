cbcd4a6315698bfbeec467fb7d54aa2156eb5bea
FoxyChipher <ageev-eldar@mail.ru>
2026-05-01 18:44:10 +0300

    throne

--- modules/programs/throne.nix
+++ modules/programs/throne.nix
@@ -1,19 +1,4 @@
-{pkgs, ...}: let
-	throne-updated =
-		pkgs.throne.overrideAttrs (oldAttrs: {
-				version = "1.1.1";
-				src =
-					pkgs.fetchFromGitHub {
-						owner = "throneproj";
-						repo = "Throne";
-						rev = "v1.1.1";
-						# You'll need to update this hash.
-						# Run `nix-prefetch-url --unpack https://github.com/throneproj/Throne/archive/refs/tags/v1.1.1.tar.gz`
-						# to get the correct SRI hash.
-						hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-					};
-			});
-in {
+{...}: {
 	programs.throne = {
 		enable = true;
 		tunMode.enable = true;
