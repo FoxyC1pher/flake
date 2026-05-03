c423ac8934ca0c7c2162c0260874b6f4294da30d
FoxyChipher <ageev-eldar@mail.ru>
2026-05-03 12:39:41 +0300

    udev

--- modules/services/udev.nix
+++ modules/services/udev.nix
@@ -8,21 +8,24 @@
 	};
 	# Отключаем энергосбережение для USB
 	services.udev.extraRules = ''
-		# Увеличиваем приоритет звуковых IRQ
-		SUBSYSTEM=="irq", ACTION=="add", ATTR{irq}=="*", ATTR{threaded}="1"
+		# --- СХЕМА ПРИОРИТЕТОВ IRQ ---
+		# Включаем threading для IRQ звуковых карт (HDA Intel и USB)
+		# Это позволяет rtkit лучше управлять их приоритетом
+		SUBSYSTEM=="irq", ATTR{threaded}="1"
 
-		# Для SSD
-		ACTION=="add|change", KERNEL=="sd*[!0-9]", ATTR{queue/scheduler}="none"
+		# --- USB AUDIO (MUSIC-BOOST Microphone) ---
+		# Отключаем агрессивное энергосбережение, которое дает щелчки
+		SUBSYSTEM=="usb", ATTR{idVendor}=="*", ATTR{idProduct}=="*", ATTR{product}=="*Usb Microphone*", ATTR{power/control}="on", ATTR{power/autosuspend}="-1"
+
+		# --- ДИСКОВАЯ ПОДСИСТЕМА ---
+		# NVMe и SSD без планировщика для минимального CPU overhead
 		ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
+		ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
+		# Только для крутящихся HDD оставляем bfq
+		ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
 
-		# Для HDD (если есть)
-		ACTION=="add|change", KERNEL=="sd*[!0-9]", ATTR{queue/scheduler}="bfq"
-
-
-		# USB audio — максимальный приоритет
-		# SUBSYSTEM=="usb", ATTR{idVendor}=="1235", ATTR{idProduct}=="*", GROUP="audio", MODE="0660", RUN+="/bin/sh -c 'echo 0 > /proc/irq/$(cat /proc/interrupts | grep -m1 "USB" | cut -d: -f1 | tr -d " ") /smp_affinity_list'"
-
-		# Gamepad / mouse / keyboard — low latency
-		KERNEL=="event*", ATTR{name}=="*Mouse*|*Keyboard*|*Gamepad*", GROUP="input", MODE="0660"
+		# --- ИНПУТЫ (Мышь/Клавиатура) ---
+		# Убираем задержки опроса для USB устройств ввода
+		ACTION=="add|change", SUBSYSTEM=="usb", ATTRS{idVendor}=="*", ATTR{bInterfaceClass}=="03", ATTR{power/control}="on", ATTR{power/autosuspend}="-1"
 	'';
 }
