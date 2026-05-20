143df6f513549d264119bee84f705b790cfb2dda
FoxyChipher <ageev-eldar@mail.ru>
2026-05-20 11:47:34 +0300

    w

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -1,238 +1,238 @@
 {
-	vars,
-	inputs,
-	pkgs,
-	...
+  vars,
+  inputs,
+  pkgs,
+  ...
 }: let
-	btoP = with pkgs;
-		if vars.hardware.nvidia.enable
-		then btop-cuda
-		else if vars.hardware.amd.enable
-		then btop-rocm
-		else btop;
-	nvtoP = with pkgs;
-		if vars.hardware.nvidia.enable
-		then nvtopPackages.nvidia
-		else if vars.hardware.amd.enable
-		then nvtopPackages.amd
-		else nvtopPackages.full;
+  btoP = with pkgs;
+    if vars.hardware.nvidia.enable
+    then btop-cuda
+    else if vars.hardware.amd.enable
+    then btop-rocm
+    else btop;
+  nvtoP = with pkgs;
+    if vars.hardware.nvidia.enable
+    then nvtopPackages.nvidia
+    else if vars.hardware.amd.enable
+    then nvtopPackages.amd
+    else nvtopPackages.full;
 in {
-	imports = [
-	];
-	environment.systemPackages = with pkgs;
-	with inputs; [
-		libsForQt5.qt5ct
-		qt6Packages.qt6ct
-		papirus-icon-theme
+  imports = [
+  ];
+  environment.systemPackages = with pkgs;
+  with inputs; [
+    libsForQt5.qt5ct
+    qt6Packages.qt6ct
+    papirus-icon-theme
 
-		# 🖥️ Core & System Essentials (Основа системы)
-		glib
-		gvfs
-		libnotify
-		zenity
-		unzip
-		zip
-		p7zip
-		xz
-		gzip
-		file
-		binutils
-		pciutils
-		usbutils
-		util-linux # Содержит taskset
-		smartmontools
-		lm_sensors
-		ethtool
-		dnsutils
-		socat
-		libmtp
+    # 🖥️ Core & System Essentials (Основа системы)
+    glib
+    gvfs
+    libnotify
+    zenity
+    unzip
+    zip
+    p7zip
+    xz
+    gzip
+    file
+    binutils
+    pciutils
+    usbutils
+    util-linux # Содержит taskset
+    smartmontools
+    lm_sensors
+    ethtool
+    dnsutils
+    socat
+    libmtp
 
-		# 📊 Monitoring & Resources (Мониторинг и анализ)
-		btoP
-		nvtoP
-		iotop
-		fastfetch
-		powertop
-		duf
-		dua
-		disktui
-		diskscan
-		diskus
-		mediainfo
-		exiftool
+    # 📊 Monitoring & Resources (Мониторинг и анализ)
+    btoP
+    nvtoP
+    iotop
+    fastfetch
+    powertop
+    duf
+    dua
+    disktui
+    diskscan
+    diskus
+    mediainfo
+    exiftool
 
-		tree
-		treecat
-		treegen
-		treemd
-		tree-from-tags
-		# tree-sitter
-		# tree-sitter-grammars.
-		treefmt
-		treesheets
-		# ⚡ Performance & Kernel (Производительность и тюнинг)
-		tuna
-		schedtool
-		scx.cscheds
-		cpufrequtils
-		irqbalance
-		numactl
-		mesa-demos
+    tree
+    treecat
+    treegen
+    treemd
+    tree-from-tags
+    # tree-sitter
+    # tree-sitter-grammars.
+    treefmt
+    treesheets
+    # ⚡ Performance & Kernel (Производительность и тюнинг)
+    tuna
+    schedtool
+    scx.cscheds
+    cpufrequtils
+    irqbalance
+    numactl
+    mesa-demos
 
-		# 🛠️ Terminal Tools & CLI (Терминальные утилиты)
-		yazi
-		eza
-		bat
-		bat-extras.core
-		fd
-		# rgrc
-		grc
-		ripgrep-all
-		fzf
-		trash-cli
-		glow
-		less
-		mcat
-		babelfish
-		pandoc
-		imagemagick
-		wget
-		aria2
-		lolcat
-		# clolcat
-		# dotacat
-		# blahaj
-		wayneko
+    # 🛠️ Terminal Tools & CLI (Терминальные утилиты)
+    yazi
+    eza
+    bat
+    bat-extras.core
+    fd
+    # rgrc
+    grc
+    ripgrep-all
+    fzf
+    trash-cli
+    glow
+    less
+    mcat
+    babelfish
+    pandoc
+    imagemagick
+    wget
+    aria2
+    lolcat
+    # clolcat
+    # dotacat
+    # blahaj
+    wayneko
 
-		# 📂 Disk & Filesystem (Диски и ФС)
-		parted
-		gparted-full
-		mtools
+    # 📂 Disk & Filesystem (Диски и ФС)
+    parted
+    gparted-full
+    mtools
 
-		# 💻 Development & Editors (Разработка и редакторы)
-		obsidian
-		git
-		gitui
-		git-credential-keepassxc
-		color-lsp
-		nixd
-		nil
-		alejandra
-		package-version-server
-		vscodium
-		zed-editor
-		# jetbrains.clion
-		# micro-full
-		gemini-cli-bin
-		#gemini-cli
-		#lagrange-tui
-		tuios
+    # 💻 Development & Editors (Разработка и редакторы)
+    obsidian
+    git
+    gitui
+    git-credential-keepassxc
+    color-lsp
+    nixd
+    nil
+    alejandra
+    package-version-server
+    vscodium
+    zed-editor
+    # jetbrains.clion
+    # micro-full
+    gemini-cli-bin
+    #gemini-cli
+    #lagrange-tui
+    tuios
 
-		# 🌐 Networking & VPN (Сеть)
-		sniffglue
-		qbittorrent
-		dropbox
-		dropbox-cli
+    # 🌐 Networking & VPN (Сеть)
+    sniffglue
+    qbittorrent
+    dropbox
+    dropbox-cli
 
-		# 🔊 Audio (Звук и его настройка)
-		pavucontrol
-		alsa-utils
-		alsa-tools
-		jack-example-tools
-		rnnoise-plugin
-		playerctl
-		mpdris2
-		pipewire
-		wireplumber
+    # 🔊 Audio (Звук и его настройка)
+    pavucontrol
+    alsa-utils
+    alsa-tools
+    jack-example-tools
+    rnnoise-plugin
+    playerctl
+    mpdris2
+    pipewire
+    wireplumber
 
-		# 🎵 Media & Streaming (Медиа контент)
-		spotify
-		youtube-tui
-		termusic
-		ytermusic
-		yt-dlp
-		ffmpeg-full
-		ffmpegthumbnailer
-		poppler
-		obs-studio-plugins.obs-vaapi
-		kew
-		# inputs.rmpc.packages.${pkgs.stdenv.hostPlatform.system}.default
+    # 🎵 Media & Streaming (Медиа контент)
+    spotify
+    youtube-tui
+    termusic
+    ytermusic
+    yt-dlp
+    ffmpeg-full
+    ffmpegthumbnailer
+    poppler
+    obs-studio-plugins.obs-vaapi
+    kew
+    # inputs.rmpc.packages.${pkgs.stdenv.hostPlatform.system}.default
 
-		# 🖼️ Wayland & Graphics (Графическая среда)
-		wayland-utils
-		xdg-utils
-		xdg-user-dirs
-		wlr-randr
-		wev
-		slurp
-		wl-clipboard
-		wl-clipboard-x11
-		wl-clip-persist
-		cliphist
-		cliphist-fuzzel-img
-		brightnessctl
-		awww
-		# inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
+    # 🖼️ Wayland & Graphics (Графическая среда)
+    wayland-utils
+    xdg-utils
+    xdg-user-dirs
+    wlr-randr
+    wev
+    slurp
+    wl-clipboard
+    wl-clipboard-x11
+    wl-clip-persist
+    cliphist
+    cliphist-fuzzel-img
+    brightnessctl
+    awww
+    # inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
 
-		# 🚀 UI Components (Интерфейс и уведомления)
-		rofi
-		rofi-polkit-agent
-		swaynotificationcenter
-		cmd-polkit
-		tuigreet
+    # 🚀 UI Components (Интерфейс и уведомления)
+    rofi
+    rofi-polkit-agent
+    swaynotificationcenter
+    cmd-polkit
+    tuigreet
 
-		# 🔐 Security (Безопасность)
-		keepassxc
-		sops
-		openssh
-		swaylock
+    # 🔐 Security (Безопасность)
+    keepassxc
+    sops
+    openssh
+    swaylock
 
-		# 🔵 Bluetooth
-		bluetui
-		bluetuith
+    # 🔵 Bluetooth
+    bluetui
+    bluetuith
 
-		# 📱 Mobile & Android (Мобильные устройства)
-		android-tools
-		android-file-transfer
-		android-translation-layer
-		extract-dtb
+    # 📱 Mobile & Android (Мобильные устройства)
+    android-tools
+    android-file-transfer
+    android-translation-layer
+    extract-dtb
 
-		# 🎮 Gaming (Игры)
-		r2modman
-		mangohud
-		yetris
-		inputs.freesmlauncher.packages.${pkgs.stdenv.hostPlatform.system}.freesmlauncher
-		jdk25
-		# 🍷 Wine & Compatibility (Совместимость с Windows)
-		wineWow64Packages.stagingFull
-		wineWow64Packages.waylandFull
-		wineWow64Packages.fonts
-		wineasio
-		winetricks
+    # 🎮 Gaming (Игры)
+    r2modman
+    mangohud
+    yetris
+    # inputs.freesmlauncher.packages.${pkgs.stdenv.hostPlatform.system}.freesmlauncher
+    jdk25
+    # 🍷 Wine & Compatibility (Совместимость с Windows)
+    wineWow64Packages.stagingFull
+    wineWow64Packages.waylandFull
+    wineWow64Packages.fonts
+    wineasio
+    winetricks
 
-		# 📨 Communication (Мессенджеры)
-		inputs.ayugram-desktop.packages.${stdenv.hostPlatform.system}.default
+    # 📨 Communication (Мессенджеры)
+    inputs.ayugram-desktop.packages.${stdenv.hostPlatform.system}.default
 
-		discordo
-		discord-gamesdk
-		arrpc
-		rustdesk-flutter
+    discordo
+    discord-gamesdk
+    arrpc
+    rustdesk-flutter
 
-		nvidia-vaapi-driver
-		libva-vdpau-driver
-		libvdpau-va-gl
+    nvidia-vaapi-driver
+    libva-vdpau-driver
+    libvdpau-va-gl
 
-		gpu-screen-recorder
-		gpu-screen-recorder-gtk
-		niri-float-sticky.packages.${stdenv.hostPlatform.system}.default
-	];
-	# ++ (
-	# 	if vars.hardware.nvidia.enable
-	# 	then [
-	# 		nvidia-vaapi-driver
-	# 		libva-vdpau-driver
-	# 		libvdpau-va-gl
-	# 	]
-	# 	else []
-	# );
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk
+    niri-float-sticky.packages.${stdenv.hostPlatform.system}.default
+  ];
+  # ++ (
+  # 	if vars.hardware.nvidia.enable
+  # 	then [
+  # 		nvidia-vaapi-driver
+  # 		libva-vdpau-driver
+  # 		libvdpau-va-gl
+  # 	]
+  # 	else []
+  # );
 }
