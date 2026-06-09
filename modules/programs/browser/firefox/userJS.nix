c1c682f3e64000f60d47ee5b454cc7fdb9cbb161
FoxyChipher <ageev-eldar@mail.ru>
2026-06-09 12:25:20 +0300

    firefox3

--- modules/programs/browser/firefox/userJS.nix
+++ modules/programs/browser/firefox/userJS.nix
@@ -24,44 +24,52 @@
 			programs.firefox = {
 				profiles.${vars.user.fullName} = {
 					settings = {
-						"browser.startup.homepage" = "about:blank";
-						"browser.newtabpage.enabled" = false;
-						"browser.search.defaultenginename" = "DuckDuckGo";
+						# --- Истинно Индусы и Интеграции ---
+						"browser.ai.control.linkPreviewKeyPoints" = "blocked";
+						"browser.ai.control.pdfjsAltText" = "blocked";
+						"browser.ai.control.smartTabGroups" = "blocked";
+						"browser.ai.control.translations" = "blocked";
+						"browser.ml.chat.page.footerBadge" = false;
+						"browser.ml.chat.page.menuBadge" = false;
+						"browser.ml.chat.provider" = "https://gemini.google.com";
+						"browser.ml.chat.shortcuts" = false;
+						"browser.ml.linkPreview.enabled" = false;
 						"extensions.pocket.enabled" = false;
-						"identity.fxaccounts.enabled" = true; # аккаунты Mozilla
-						"toolkit.legacyUserProfileCustomizations.stylesheets" = true; # userChrome
-						"sidebar.verticalTabs" = false;
-						"sidebar.main.tools" = "none";
-						"browser.settings-redesign.promo.dismissed" = true;
-						"browser.tabs.tabmanager.enabled" = true;
-						"browser.tabs.inTitlebar" = false;
-						"browser.tabs.closeWindowWithLastTab" = false;
-						"browser.tabs.allow_transparent_browser" = true;
+
+						# --- Внешний вид и темы ---
 						"browser.compactmode.show" = true;
-						# "browser.uidensity" = 1; # compact
+						"browser.settings-redesign.promo.dismissed" = true;
 						"browser.smartwindow.nova.enabled" = false;
-						"svg.context-properties.content.enabled" = true;
-						"layers.acceleration.force-enabled" = true;
+						"browser.tabs.allow_transparent_browser" = true;
+						"browser.tabs.closeWindowWithLastTab" = false;
+						"browser.tabs.inTitlebar" = false;
 						"browser.tabs.tabMinWidth" = 0;
 						"browser.toolbars.bookmarks.visibility" = "always";
-
-						"widget.wayland.opaque-by-default" = false;
-						"widget.transparent-windows" = true;
+						"layout.css.prefers-color-scheme.content-override" = 0;
 						"layout.css.prefers-reduced-transparency.enabled" = false;
-						"widget.gtk.rounded-bottom-corners.enabled" = true;
-						"sidebar.revamp" = false;
-						"privacy.donottrackheader.enabled" = true;
-						"sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
-						"sidebar.visibility" = "hide-sidebar";
-						"media.ffmpeg.vaapi.enabled" = true;
-						"media.hardware-video-decoding.force-enabled" = true;
-						"media.rdd-ffmpeg.enabled" = rddEnabled;
+						"svg.context-properties.content.enabled" = true;
+						"toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+						# --- Загрузки и Формы ---
+						"browser.download.autohideButton" = false;
+						"browser.download.panel.shown" = true;
+						"browser.formfill.enable" = false;
+
+						# --- Шрифты ---
+						"font.minimum-size.x-cyrillic" = vars.theme.font.size;
+						"font.minimum-size.x-western" = vars.theme.font.size;
+						"font.size.monospace.x-cyrillic" = vars.theme.font.size;
+						"font.size.monospace.x-western" = vars.theme.font.size;
+						"font.size.variable.x-cyrillic" = vars.theme.font.size;
+						"font.size.variable.x-western" = vars.theme.font.size;
+
+						# --- Графика и Аппаратное Ускорение ---
 						"browser.preferences.defaultPerformanceSettings.enabled" = false;
-						"gfx.webrender.all" = true;
-						"gfx.webrender.enabled" = true;
 						"gfx.webgpu.ignore-blocklist" = true;
+						"gfx.webrender.all" = true;
 						"gfx.webrender.compositor" = true;
 						"gfx.webrender.compositor.force-enabled" = true;
+						"gfx.webrender.enabled" = true;
 						"gfx.webrender.svg-filter-effects" = true;
 						"gfx.webrender.svg-filter-effects.also-convert-css-filters" = true;
 						"gfx.webrender.svg-filter-effects.also-use-for-docshell-fecolormatrix" = false;
@@ -89,61 +97,66 @@
 						"gfx.webrender.unaccelerated-widget.force" = false;
 						"gfx.webrender.use-optimized-shaders" = true;
 						"gfx.x11-egl.force-enabled" = true;
-						"widget.dmabuf.force-enabled" = true;
-						"layout.css.prefers-color-scheme.content-override" = 0;
+						"layers.acceleration.force-enabled" = true;
+						"media.ffmpeg.vaapi.enabled" = true;
+						"media.hardware-video-decoding.force-enabled" = true;
+						"media.rdd-ffmpeg.enabled" = rddEnabled;
+
+						# --- Синхронизация и аккаунт ---
+						"identity.fxaccounts.account.device.name" = "${vars.user.name}@${vars.host}";
+						"identity.fxaccounts.enabled" = true;
+
+						# --- Страница новой вкладки и домашняя страница ---
+						"browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+						"browser.newtabpage.activity-stream.feeds.snippets" = false;
+						"browser.newtabpage.activity-stream.feeds.topsites" = false;
+						"browser.newtabpage.activity-stream.nova.enabled" = false;
+						"browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
+						"browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
+						"browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+						"browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
+						"browser.newtabpage.activity-stream.showSearch" = false;
+						"browser.newtabpage.activity-stream.showSponsored" = false;
+						"browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+						"browser.newtabpage.activity-stream.system.showSponsored" = false;
+						"browser.newtabpage.enabled" = false;
+						"browser.startup.homepage" = "about:blank";
+						"browser.topsites.contile.enabled" = false;
+
+						# --- Конфиденциальность и Защита ---
+						"browser.aboutConfig.showWarning" = false;
 						"browser.contentblocking.category" = {
 							Value = "strict";
 							Status = "locked";
 						};
-
-						"browser.aboutConfig.showWarning" = false;
-						"browser.ai.control.linkPreviewKeyPoints" = "blocked";
-						"browser.ai.control.pdfjsAltText" = "blocked";
-						"browser.ai.control.smartTabGroups" = "blocked";
-						"browser.ai.control.translations" = "blocked";
-
-						"browser.bookmarks.showMobileBookmarks" = false;
-						"browser.ctrlTab.sortByRecentlyUsed" = true;
-
-						"font.minimum-size.x-cyrillic" = vars.theme.font.size;
-						"font.minimum-size.x-western" = vars.theme.font.size;
-						"font.size.monospace.x-cyrillic" = vars.theme.font.size;
-						"font.size.monospace.x-western" = vars.theme.font.size;
-						"font.size.variable.x-cyrillic" = vars.theme.font.size;
-						"font.size.variable.x-western" = vars.theme.font.size;
-
-						"identity.fxaccounts.account.device.name" = "${vars.user.name}@${vars.host}";
-
-						"browser.download.autohideButton" = false;
-						"browser.download.panel.shown" = true;
-
-						"browser.ml.chat.page.footerBadge" = false;
-						"browser.ml.chat.page.menuBadge" = false;
-						"browser.ml.chat.provider" = "https://gemini.google.com";
-						"browser.ml.chat.shortcuts" = false;
-						"browser.ml.linkPreview.enabled" = false;
-
 						"extensions.screenshots.disabled" = true;
-						"browser.topsites.contile.enabled" = false;
-						"browser.formfill.enable" = false;
-						"browser.search.suggest.enabled" = true;
-						"browser.search.suggest.enabled.private" = false;
-						"browser.urlbar.suggest.searches" = false;
-						"browser.newtabpage.activity-stream.showSearch" = false;
-						"browser.urlbar.showSearchSuggestionsFirst" = false;
-						"browser.newtabpage.activity-stream.nova.enabled" = false;
-						"browser.newtabpage.activity-stream.feeds.topsites" = false;
-						"browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-						"browser.newtabpage.activity-stream.feeds.snippets" = false;
-						"browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-						"browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
-						"browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
-						"browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
-						"browser.newtabpage.activity-stream.showSponsored" = false;
-						"browser.newtabpage.activity-stream.system.showSponsored" = false;
-						"browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+						"privacy.donottrackheader.enabled" = true;
+
+						# --- Боковая Панель ---
+						"sidebar.main.tools" = "none";
+						"sidebar.revamp" = false;
+						"sidebar.verticalTabs" = false;
+						"sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+						"sidebar.visibility" = "hide-sidebar";
+
+						# --- Переводчик ---
 						"browser.translations.automaticallyPopup" = false;
 						"browser.translations.enable" = false;
+
+						# --- Адресная строка и поиск ---
+						"browser.ctrlTab.sortByRecentlyUsed" = true;
+						"browser.search.defaultenginename" = "DuckDuckGo";
+						"browser.search.suggest.enabled" = true;
+						"browser.search.suggest.enabled.private" = false;
+						"browser.tabs.tabmanager.enabled" = true;
+						"browser.urlbar.showSearchSuggestionsFirst" = false;
+						"browser.urlbar.suggest.searches" = false;
+
+						# --- Wayland и Управление Окнами ---
+						"widget.dmabuf.force-enabled" = true;
+						"widget.gtk.rounded-bottom-corners.enabled" = true;
+						"widget.transparent-windows" = true;
+						"widget.wayland.opaque-by-default" = false;
 					};
 				};
 			};
