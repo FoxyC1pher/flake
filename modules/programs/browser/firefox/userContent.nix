5a2a5fc66ab9fb515cfea51326e43a9b26433cdd
FoxyChipher <ageev-eldar@mail.ru>
2026-06-09 12:36:20 +0300

    firefox5

--- modules/programs/browser/firefox/userContent.nix
+++ modules/programs/browser/firefox/userContent.nix
@@ -11,14 +11,14 @@
 			programs.firefox = {
 				profiles.${vars.user.fullName} = {
 					userContent = ''
-						/* ── Полный сброс холста для всех внешних сайтов ───────────── */
+						/* Полный сброс холста для всех сайтов */
 						@-moz-document regexp("^(?!moz-extension://|about:).*$") {
 						  :root, :root::before, :root::after, html, body {
 						    background-color: transparent !important;
 						    background-image: none !important;
 						  }
 						}
-						/* ── Скроллбар под стиль системы ────────────────────────────── */
+						/* Скроллбар под стиль системы */
 						* {
 						  scrollbar-color: ${t.ui.selection} ${t.ui.main} !important;
 						}
