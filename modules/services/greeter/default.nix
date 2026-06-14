7f242db0cdd4da58a2459ba073b8179aea750e12
FoxyChipher <ageev-eldar@mail.ru>
2026-06-14 08:00:05 +0300

    greeters

--- modules/services/greeter/default.nix
+++ modules/services/greeter/default.nix
@@ -1,5 +1,12 @@
 {...}: {
 	imports = [
-		./dms-greeter.nix
+		./cosmic-greeter.nix
+		# ./dms-greeter.nix
+		# ./gdm.nix
+		# ./lemurs.nix
+		# ./ly.nix
+		# ./plasma-login-manager.nix
+		# ./sddm.nix
+		# ./tuigreet.nix
 	];
 }
