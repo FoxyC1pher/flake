fc3b44e15e05dce0a0e20151fa5fd58d89d4df4c
FoxyChipher <ageev-eldar@mail.ru>
2026-06-18 17:33:15 +0300

    бат синс айв сиин тзис факдап щит айв тхинк ай чанджд май майнд

--- lib/utils.nix
+++ lib/utils.nix
@@ -1,7 +1,6 @@
-# lib/utils.nix
 {lib, ...}: rec {
-	# Рекурсивно ищет модуль по имени в любой поддиректории basePath.
-	# Возвращает список найденных путей (директории с default.nix или .nix-файлы).
+	# Рекурсивный поиск модуля по имени.
+	# Если в поддиректории найден default.nix, глубокий поиск внутри неё прекращается.
 	findDeep = basePath: name: let
 		entries = builtins.readDir basePath;
 
@@ -10,12 +9,22 @@
 
 		directMatches =
 			lib.optional
-			(builtins.pathExists asDir && builtins.pathExists (asDir + "/default.nix"))
-			asDir
+			(builtins.pathExists asDir && builtins.pathExists "${asDir}/default.nix")
+			"${asDir}/default.nix"
 			++ lib.optional (builtins.pathExists asFile) asFile;
 
-		# Рекурсия во все поддиректории (кроме самой `name`, чтобы не войти повторно)
-		subdirs = lib.filterAttrs (n: t: t == "directory" && n != name) entries;
+		# Фильтруем директории для рекурсии: исключаем саму искомую 'name'
+		# и пропускаем те папки, которые содержат собственный default.nix (чтобы избежать заглядывания внутрь изолированных модулей)
+		subdirs =
+			lib.filterAttrs (
+				n: t:
+					t
+					== "directory"
+					&& n != name
+					&& !(builtins.pathExists (basePath + "/${n}/default.nix"))
+			)
+			entries;
+
 		subMatches =
 			lib.concatMap
 			(subdir: findDeep (basePath + "/${subdir}") name)
@@ -23,7 +32,7 @@
 	in
 		directMatches ++ subMatches;
 
-	# Импорт категории: все .nix-файлы и все поддиректории с default.nix
+	# Возвращает список путей файлов/модулей внутри конкретной категории
 	importCategoryDir = dirPath: let
 		entries = builtins.readDir dirPath;
 
@@ -43,37 +52,36 @@
 				name: type:
 					type
 					== "directory"
-					&& builtins.pathExists (dirPath + "/${name}/default.nix")
+					&& builtins.pathExists "${dirPath}/${name}/default.nix"
 					&& !(lib.hasPrefix "_" name)
 			)
 			entries;
 	in
-		map (name: import (dirPath + "/${name}")) (builtins.attrNames nixFiles)
-		++ map (name: import (dirPath + "/${name}/default.nix")) (builtins.attrNames subdirs);
+		(map (name: "${dirPath}/${name}") (builtins.attrNames nixFiles))
+		++ (map (name: "${dirPath}/${name}/default.nix") (builtins.attrNames subdirs));
 
-	# Основная функция: принимает список имён программ/категорий,
-	# находит каждую где угодно в поддереве programsBase.
-	importPrograms = programsBase: items:
-		lib.concatMap (
-			item: let
-				directDir = programsBase + "/${item}";
-				directFile = programsBase + "/${item}.nix";
-			in
-				if builtins.pathExists directDir
-				then
-					if builtins.pathExists (directDir + "/default.nix")
-					then [import (directDir + "/default.nix")] # один модуль
-					else importCategoryDir directDir # категория
-				else if builtins.pathExists directFile
-				then [import directFile]
-				else
-					# Не нашли напрямую — ищем рекурсивно
-					map (
-						p:
-							if builtins.pathExists (p + "/default.nix")
-							then import (p + "/default.nix")
-							else import p
-					) (findDeep programsBase item)
-		)
-		items;
+	# Основная функция импорта программ с жесткой дедупликацией путей
+	importPrograms = programsBase: items: let
+		allPaths =
+			lib.concatMap (
+				item: let
+					directDir = programsBase + "/${item}";
+					directFile = programsBase + "/${item}.nix";
+				in
+					if builtins.pathExists directDir
+					then
+						if builtins.pathExists "${directDir}/default.nix"
+						then ["${directDir}/default.nix"]
+						else importCategoryDir directDir
+					else if builtins.pathExists directFile
+					then [directFile]
+					else
+						# Если прямой путь не найден, запускаем контролируемый findDeep
+						findDeep programsBase item
+			)
+			items;
+
+		uniquePaths = lib.unique allPaths;
+	in
+		map (path: import path) uniquePaths;
 }
